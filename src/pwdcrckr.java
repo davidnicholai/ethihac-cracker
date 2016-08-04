4db288f50522d7fcc5bf76e7700f7eb8ce73f6dd
David Lim <nicholai.david@gmail.com>
2016-08-04 20:02:07 +0800

    Asks for file inputs

--- src/pwdcrckr.java
+++ src/pwdcrckr.java
@@ -5,12 +5,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class pwdcrckr {
 
-    private final String FILEPATH_WORST_PASSWORDS = getClass().getResource("500-worst-passwords.txt").getPath();
-    private final String FILEPATH_PASSWD = getClass().getResource("passwd.txt").getPath();
-    private final String FILEPATH_SHADOW = getClass().getResource("shadow.txt").getPath();
+    private final String PATH_TO_ASSETS_DIR = "./assets/";
+    private String FILEPATH_WORST_PASSWORDS = PATH_TO_ASSETS_DIR + "500-worst-passwords.txt";
+    private String FILEPATH_PASSWD = PATH_TO_ASSETS_DIR + "passwd.txt";
+    private String FILEPATH_SHADOW = PATH_TO_ASSETS_DIR + "shadow.txt";
     private final static String LINE_SEPARATOR = "\r\n";
     private final static String USERINFO_SEPARATOR = ":";
 
@@ -20,27 +22,73 @@ public class pwdcrckr {
     private File fileOutput;
 
     public static void main(String[] args) {
-        System.out.println("Starting now");
-        final long startTime = System.currentTimeMillis();
-
         pwdcrckr pwdcrckr = new pwdcrckr();
         try {
+            pwdcrckr.debugger();
+            System.out.println("\nStarting now.\n");
+            final long startTime = System.currentTimeMillis();
             pwdcrckr.getWorstPasswords();
             pwdcrckr.getUsers();
             pwdcrckr.getPasswords();
             pwdcrckr.performCracking();
             pwdcrckr.displayResults();
+            final long duration = System.currentTimeMillis() - startTime;
+            System.out.println("\nExecution time: " + (duration / 1000) + " seconds.\n\nDumped results in results.txt");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        final long duration = System.currentTimeMillis() - startTime;
-        System.out.println("Execution time: " + (duration / 1000) + " seconds");
-
     }
 
     private pwdcrckr() {
         fileOutput = new File("./results.txt");
+    }
+
+    private void debugger() {
+        String inputWorstPasswords, inputPasswd, inputShadow;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("    ___________ __  _____   ______\n   / ____/__  // / / /   | / ____/" +
+                "\n  / __/   /_ </ /_/ / /| |/ /     " +
+                "\n / /___ ___/ / __  / ___ / /___   " +
+                "\n/_____//____/_/ /_/_/  |_\\____/   \n");
+
+        System.out.println("Welcome to the ETHIHAC Cracker by David Lim, Kurt Ting, and Ivan Demabildo.");
+        System.out.println("If you want to use custom passwd and shadow files, place them in the program's assets directory now.");
+        System.out.println("TIP: You can leave the input fields blank to use the default files.");
+
+        System.out.print("\nEnter worst passwords file (500-worst-passwords.txt): ");
+        inputWorstPasswords = in.nextLine();
+
+        System.out.print("Enter passwd file (passwd.txt): ");
+        inputPasswd = in.nextLine();
+
+        System.out.print("Enter shadow file (shadow.txt): ");
+        inputShadow = in.nextLine();
+
+        if (!inputWorstPasswords.isEmpty()) {
+            FILEPATH_WORST_PASSWORDS = PATH_TO_ASSETS_DIR + inputWorstPasswords;
+            if (!new File(FILEPATH_WORST_PASSWORDS).isFile()) {
+                System.out.println("Could not find " + inputWorstPasswords + " in the assets directory. Exiting.");
+                System.exit(-1);
+            }
+        }
+
+        if (!inputPasswd.isEmpty()) {
+            FILEPATH_PASSWD = PATH_TO_ASSETS_DIR + inputPasswd;
+            if (!new File(FILEPATH_PASSWD).isFile()) {
+                System.out.println("Could not find " + inputPasswd + " in the assets directory. Exiting.");
+                System.exit(-1);
+            }
+        }
+
+        if (!inputShadow.isEmpty()) {
+            FILEPATH_SHADOW = PATH_TO_ASSETS_DIR + inputShadow;
+            if (!new File(FILEPATH_SHADOW).isFile()) {
+                System.out.println("Could not find " + inputShadow + " in the assets directory. Exiting.");
+                System.exit(-1);
+            }
+        }
+
     }
 
     private void getWorstPasswords() throws IOException {
@@ -102,7 +150,7 @@ public class pwdcrckr {
                 all += result + LINE_SEPARATOR;
             }
         }
-        System.out.println("=====      END OF RESULTS      =====");
+        System.out.println("========== END OF RESULTS ==========");
 
         FileUtils.writeStringToFile(fileOutput, all, Charsets.UTF_8, false);
         // the false means don't append to current file
